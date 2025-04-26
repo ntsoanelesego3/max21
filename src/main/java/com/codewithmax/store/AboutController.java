d23cbea4de72ffd87da8ff1e04bf3d2cf0a38bfb
ntsoanelesego3 <ntsoanelesego3@gmail.com>
2025-04-26 20:03:12 +0200

    Spring project

---
+++ src/main/java/com/codewithmax/store/AboutController.java
@@ -0,0 +1,14 @@
+package com.codewithmax.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutController {
+        @RequestMapping("/about")
+        public String about(){
+            return "about.html";
+        }
+    }
+
+
