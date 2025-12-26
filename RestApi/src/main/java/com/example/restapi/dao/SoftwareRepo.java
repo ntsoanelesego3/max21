db9e923b44e9f1363f8fb4e0ad7da9a39e3aa52c
ntsoanelesego3 <165815506+ntsoanelesego3@users.noreply.github.com>
2025-12-26 14:21:21 +0200

    Delete RestApi directory
    not functioning correctly

--- RestApi/src/main/java/com/example/restapi/dao/SoftwareRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.restapi.dao;
-
-import com.example.restapi.api.SoftwareEngineer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SoftwareRepo extends JpaRepository<SoftwareEngineer, Integer> {
-}
